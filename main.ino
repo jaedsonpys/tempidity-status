91d316999a9cb6b2ee386221b3cda19aea8f7f96
Jaedson Silva <contacanal798@gmail.com>
2022-10-11 01:22:05 -0300

    adding medium alert with buzzer and blue LED

--- main.ino
+++ main.ino
@@ -1,10 +1,13 @@
 #include "Arduino.h"
 
 const int redLed = 19;
-const int greenLed = 3;
+const int greenLed = 15;
 const int blueLed = 4;
 
 const int buzzerPin = 23;
+const int buzzerFreq = 1500;
+
+void mediumAlert();
 
 void setup() {
     pinMode(redLed, OUTPUT);
@@ -16,4 +19,17 @@ void setup() {
 
 void loop() {
 
+}
+
+void mediumAlert() {
+    for(int i = 0; i < 1; i++) {
+        analogWrite(blueLed, 255);
+        tone(buzzerPin, buzzerFreq);
+        
+        delay(500);
+
+        noTone(buzzerPin);
+        analogWrite(blueLed, 0);
+        delay(1000);
+    }
 }
