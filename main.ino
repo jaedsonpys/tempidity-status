9e7c7c10942f64ccbbae6144c33e4d8f2a6ef1ef
Jaedson Silva <contacanal798@gmail.com>
2022-10-11 01:08:17 -0300

    define pin mode to RGB leds and buzzer

--- main.ino
+++ main.ino
@@ -7,7 +7,11 @@ const int blueLed = 4;
 const int buzzerPin = 23;
 
 void setup() {
+    pinMode(redLed, OUTPUT);
+    pinMode(greenLed, OUTPUT);
+    pinMode(blueLed, OUTPUT);
 
+    pinMode(buzzerPin, OUTPUT);
 }
 
 void loop() {
